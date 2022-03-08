3af9a5dd5c47979addc58ce53be38f8bd08c7a07
gba1990 <gba1880@gmail.com>
2022-03-08 15:52:19 +0100

    FIX: Dodge enable/disable

--- AI/Modules/AIModuleShaman.lua
+++ AI/Modules/AIModuleShaman.lua
@@ -16,8 +16,31 @@ function AIModuleShaman:new(o, ai)
     o.dodgePercentChance = 100 -- % of chance to perform a dodge, for example it can increment as the game advances so "ai gets better at dodging" or viceversa
     o.dodgeIntervalBetweenDodges = 15 -- Once a dodge takes place how long before the next (set to at least 12 or so)
 
-    handlerFunctions.AIShamanDodgeController.usingOnCreateThing(o)
     o:enable()
 
     return o
+end
+
+function AIModuleShaman:doDodge()
+    handlerFunctions.AIShamanDodgeController.usingOnCreateThing(self)
+end
+
+function AIModuleShaman:dontDoDodge()
+    unsubscribe_OnCreateThing(self.dodgeControllerSubscriptionIndex)
+end
+
+function AIModuleShaman:enable()
+    if (self.isEnabled) then
+        return
+    end
+    self:setEnabled(true)
+    self:doDodge()
+end
+
+function AIModuleShaman:disable()
+    if (not self.isEnabled) then
+        return
+    end
+    self:setEnabled(false)
+    self:dontDoDodge()
 end
